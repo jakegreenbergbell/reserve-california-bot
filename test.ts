b12b60fb0c08bddb4a35c2c306b4a37cebf149a5
jakegreenbergbell <32340594+jakegreenbergbell@users.noreply.github.com>
2022-12-23 22:39:14 -0800

    First commit
    - Add basic getCampground async route that gets campground availability
       for a specific start and end date.
    
    - Add checking if there is a campground available for a specified time.
    
    - For now this is just for a single campground. In the future it will be for all.
    
    - In the future you will get an email if there is a campground of your choosing.

---
+++ test.ts
@@ -0,0 +1,73 @@
+import axios, { AxiosResponse } from 'axios';
+
+const carpinteriaSantaCruz = {
+    id: 358
+};
+
+interface Campground {
+    id: number;
+}
+
+interface Options {
+    startDate: string;
+    endDate: string;
+
+}
+
+
+const getCampground = async (campground: Campground, options: Options): Promise<null | AxiosResponse> =>  {
+    console.log(`Searching campground ${campground.id}`);
+    try {
+        return await axios({
+            method: 'post',
+            url: 'https://calirdr.usedirect.com/RDR/rdr/search/grid', 
+            headers: {
+                "Content-Type": "application/json",
+                "Accept-Encoding": "gzip,deflate,compress" 
+              },
+            data: {
+                "StartDate": options.startDate,
+                "EndDate": options.endDate,
+                "UnitSort": "orderby",
+                "FacilityId": campground.id,
+            }
+    
+        })
+        .then((response) => {
+            return response.data;
+        });
+    } catch (err) {
+        return err;
+    }
+}
+
+interface campgroundAvailabilities {
+    datesAvailable: ReadonlyArray<Date>
+}
+
+const campgroundIsAvailable = (campgroundAvailability, options) => {
+    const campgroundDates = campgroundAvailability["Slices"];
+    const isFilledOnOneDayOfRange = Object.keys(campgroundDates).map((key) => campgroundDates[key]).filter((date) => !date["IsFree"]);
+    return isFilledOnOneDayOfRange.length === 0;
+}
+
+const main = async ()  => {
+    // Get campground availability
+    const availability = await getCampground(carpinteriaSantaCruz, {startDate: "2023-01-30", endDate: "2023-01-31"});
+    const campgrounds = availability["Facility"]["Units"];
+
+    // Turn Object of campground id keys into array, [ {campground}, {campground}, etc. ]
+    const campgroundsArray = Object.keys(campgrounds).map((key) => campgrounds[key]);
+    
+    // Filter the campgrounds by the ones that are available
+    const availableCampgroundsDataNeeded = 
+        campgroundsArray
+        .filter((campground) => campgroundIsAvailable(campground, {})) // Add options for filtering later
+        .map((campground) => ({name: campground["Name"]})) // Get only name
+    
+    console.log("Available campgrounds at carpinteria:")
+    console.log(availableCampgroundsDataNeeded);
+}
+
+main();
+
